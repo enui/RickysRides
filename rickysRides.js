df5330030ce8c79583948e02af349a77a0670734
Enui <enui.williams@gmail.com>
2019-10-15 22:38:57 +1300

    Fixes

--- rickysRides.js
+++ rickysRides.js
@@ -1,26 +1,43 @@
-// JSSSSSSSSSSSSSSSSSSSSSSS
+// Javascript
 
-// ? What is this
-function shrug() {
+// Event listeners
 
+// 
+
+// Insurance $20 a day
+// $50 fee
+// Add extras
+// Price per day
+// Total cost
+
+function costs(form) {
+    var totalCost = 0;
+    var BOOKINGFEE = 50;
+    // TODO: Calculate days using 2 dates
+    // Calculating vehicle selected
+    for (var i = 0; i < form.elements.length; i++){
+        if(form.elements[i].type == "radio") {
+            if (form.elements[i].checked == true) {
+                vehicleChoice = form.elements[i].value;
+                vehicleCost = form.elements[i].dataset.price;
+                vehicleOutput.innerHTML = vehicleChoice;
+            }
+        }
+    }
 }
 
-// ! This code is broken
-function broke() {
+// Confirm booking, uploads results to firebase
+function confirmBooking() {
+    var database = firebase.database();
+    var bookingRef = database.ref('bookings');
+    var name = nameInput.value;
+    var email = emailInput.value;
+    var cellphone = cellphoneInput.vale;
+    var age = ageInput.value;
+    var comment = commentInput.value;
+    // TODO: Finish filling in the JSON file
+    var bookingsEntry = {
 
-}
-
-// TODO: Fix this function
-function fix() {
-
-}
-
-//  *HMMMMMMMMMM
-function important() {
-
-}
-
-// Normal comment
-function normal() {
-    
+    }
+    bookingRef.push(bookingsEntry);
 }
